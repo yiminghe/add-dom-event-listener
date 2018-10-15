14cefb7e7f3a534296d87df59dea4e7f0658ae9a
zombiej <smith3816@gmail.com>
2018-10-15 11:58:32 +0800

    support option

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import EventObject from './EventObject';
 
-export default function addEventListener(target, eventType, callback) {
+export default function addEventListener(target, eventType, callback, option) {
   function wrapCallback(e) {
     const ne = new EventObject(e);
     callback.call(target, ne);
   }
 
   if (target.addEventListener) {
-    target.addEventListener(eventType, wrapCallback, false);
+    target.addEventListener(eventType, wrapCallback, option || false);
     return {
       remove() {
-        target.removeEventListener(eventType, wrapCallback, false);
+        target.removeEventListener(eventType, wrapCallback, option || false);
       },
     };
   } else if (target.attachEvent) {
